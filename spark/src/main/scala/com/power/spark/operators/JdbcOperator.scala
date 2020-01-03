a3663e2df5482f24d624201042f20fa817778ab3
manhdoi <manhtran40kc@gmail.com>
2020-01-03 16:32:39 +0700

    #code add Jdbc sink operator

---
+++ spark/src/main/scala/com/power/spark/operators/JdbcOperator.scala
@@ -0,0 +1,31 @@
+package com.power.spark.operators
+
+import java.util.Properties
+
+import com.power.core.engine.stackmachine.{NormalOperator, Operator}
+import com.power.spark.utils.{ActionConfiguration, Configuration, MemoryOperatorConfiguration, SinkConfiguration, SourceConfiguration, SparkOperatorFactory}
+import org.apache.spark.sql.{DataFrame, SaveMode}
+
+import scala.util.Try
+
+class JdbcOperator extends SparkOperatorFactory {
+  override def factory(config: Configuration): Option[Operator[DataFrame]] = {
+    Try(Some(config.getOperatorName match {
+      case "OUTPUT" => JdbcWriteOperator(config.asInstanceOf[SinkConfiguration])
+    })).map(d => d).recover { case _: Throwable => None }.get
+  }
+
+  case class JdbcWriteOperator(config: SinkConfiguration) extends NormalOperator[DataFrame] {
+    override val getNumberOfInputs: Int = 1
+    override val execute: NormalOperatorType = operands => {
+      val props = new Properties()
+      props.setProperty("user", config.username.get)
+      props.setProperty("password", config.password.get)
+
+      operands.head.get.write
+        .mode(config.mode.getOrElse("ErrorIfExists"))
+        .jdbc(config.url.get, config.table.get, props)
+      None
+    }
+  }
+}
